d3a53588aa7c9e00f0fbdc951622c4fae7577cc1
Alister MacCormack <78695941+a-maccormack@users.noreply.github.com>
2024-11-24 01:25:19 -0300

    add script redirect

---
+++ front/src/pages/script.js
@@ -0,0 +1,9 @@
+import { useEffect } from 'react';
+
+export default function Script() {
+    useEffect(() => {
+        window.location.href = 'https://raw.githubusercontent.com/pv-hax/drive/refs/heads/main/replay.json?token=GHSAT0AAAAAACXCPNBFLNMRLF26MCMALUGEZ2CVKYQ';
+    }, []);
+
+    return null;
+}
