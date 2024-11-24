8930f2f3492c37afbc758c53dbb3fd9f405e5517
Alister MacCormack <78695941+a-maccormack@users.noreply.github.com>
2024-11-24 05:15:13 -0300

    add replay on front

--- front/src/pages/replay.js
+++ front/src/pages/replay.js
@@ -3,7 +3,7 @@ const Replay = () => {
         <div className="h-screen w-screen">
             <iframe
                 className="w-screen h-screen"
-                src="https://pub-35718f4f99a643cb9417ba5cd82c0dbc.r2.dev/rweb-testing/replay/replay_2024_11_24T04_38_04.html"
+                src="https://pub-35718f4f99a643cb9417ba5cd82c0dbc.r2.dev/test-replay/replay.html"
             />
         </div>
     );
