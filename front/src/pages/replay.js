18b947168f5f013fe63ec2b492dfd5edfe61e288
Alister MacCormack <78695941+a-maccormack@users.noreply.github.com>
2024-11-24 02:18:30 -0300

    https poc

--- front/src/pages/replay.js
+++ front/src/pages/replay.js
@@ -3,7 +3,7 @@ const Replay = () => {
         <div className="h-screen w-screen">
             <iframe
                 className="w-screen h-screen"
-                src="http://100.26.197.252:8000/replay/replay_2024_11_24T04_38_04.html"
+                src="https://pub-35718f4f99a643cb9417ba5cd82c0dbc.r2.dev/rweb-testing/replay/replay_2024_11_24T04_38_04.html"
             />
         </div>
     );
