195b4fc2e8d8675b995759a277a48815b806ce4d
Alister MacCormack <78695941+a-maccormack@users.noreply.github.com>
2024-11-24 02:02:29 -0300

    add replay poc

---
+++ front/src/pages/replay.js
@@ -0,0 +1,12 @@
+const Replay = () => {
+    return (
+        <div className="h-screen w-screen">
+            <iframe
+                className="w-screen h-screen"
+                src="http://100.26.197.252:8000/replay/replay_2024_11_24T04_38_04.html"
+            />
+        </div>
+    );
+}
+
+export default Replay;
